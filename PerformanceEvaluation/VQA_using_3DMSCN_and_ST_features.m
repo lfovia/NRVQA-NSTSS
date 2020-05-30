0db8f9a6a9ac52298f90242c609a6920e20825a1
LFOVIA <sumohana@iith.ac.in>
2020-05-30 12:21:58 +0530

    Add files via upload

---
+++ PerformanceEvaluation/VQA_using_3DMSCN_and_ST_features.m
@@ -0,0 +1,28 @@
+clc;clear all; close all;
+addpath("./logisticfitting");
+addpath("./FeatureExtraction");
+scores = importdata('subjective_scores.txt');
+DMOS = scores;
+load('3dmscn_features.mat');
+load('spatiotemporal_inphase_features.mat.mat');
+load('spatiotemporal_quadrature_features.mat');
+
+features  = [features_set features_set_cos features_set_sin];
+no_of_iterations = 100;
+TrainRatio = 0.8;
+TestRatio = 0.2;
+ValidationRatio = 0;
+temp=[];
+for iter = 1:1:no_of_iterations
+[trainInd,valInd,testInd] = dividerand(size(features_set,1),TrainRatio,ValidationRatio,TestRatio);
+mdl = fitrsvm(features(trainInd,:),DMOS(trainInd),'KernelFunction','rbf','KernelScale','auto','Standardize',true,'CacheSize','maximal');
+y_cap = predict(mdl,features(testInd,:));
+mos_cap = DMOS(testInd);
+temp(iter,:) = calculatepearsoncorr(y_cap,mos_cap);
+
+
+
+
+median(temp)
+iter
+end
